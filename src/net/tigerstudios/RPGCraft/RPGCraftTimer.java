1a91e57b3fba534c3eea89896d4242021ca2add0
Michael Rhodes <michael@tigerstudios.net>
2012-11-07 21:07:28 -0400

    dd

---
+++ src/net/tigerstudios/RPGCraft/RPGCraftTimer.java
@@ -0,0 +1,18 @@
+package net.tigerstudios.RPGCraft;
+
+import org.bukkit.Bukkit;
+
+
+public class RPGCraftTimer implements Runnable {
+		
+	
+	RPGCraftTimer()	{}
+	
+	@Override
+	public void run()
+	{		
+		// Monster Spawning Control
+		// Player Management (ie. Hunger, Thirst, etc)
+		// Crop growing, other Tradeskill updating
+	}
+}
